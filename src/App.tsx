fade7648a81633758e815f7ab713b8d81f210cf0
Akshay Chaudasama <chudasamaakshay45@gmail.com>
2023-07-23 15:23:20 +0530

    Made changes to display line graph and to aggregate duplicated data

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -21,7 +22,9 @@ class App extends Component<{}, IState> {
     this.state = {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
+      // Graph will not be displayed until user click Start Streaming Data button
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,18 +32,30 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if(this.state.showGraph) {
+      // Show Graph only after user click Start Streaming Data button
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let renderCount = 0;
+    const renderInterval = setInterval(() => {
+      // Fetch data for 1000 times from server after every 100 milliseconds
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state after receiving new data from server
+        this.setState({data: serverResponds, showGraph: true });
+      });
+      renderCount++;
+      if(renderCount > 1000) {
+        // Clear render interval once data fetched more than 1000 times
+        clearInterval(renderInterval);
+      }
+    }, 100);
+    
   }
 
   /**
